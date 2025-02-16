4bd5f5a001faef9f8c22ee5b24256aed478cf173
dimaivanina <dimaivanina08@gmail.com>
2025-02-17 00:29:30 +0200

    fix app routes and build directory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,8 @@ import {FilmDetailComponent} from "./film-detail/film-detail.component";
 import {CharacterInfoComponent} from "./character-info/character-info.component";
 
 export const routes: Routes = [
+  {path: '', redirectTo: '/home-page', pathMatch: 'full'}, // перенаправлення на home-page
   {path: 'home-page', component: HomePageComponent},
   {path: 'home-page/:id', component: FilmDetailComponent},
   {path: 'character/:name', component: CharacterInfoComponent}
-
 ];
